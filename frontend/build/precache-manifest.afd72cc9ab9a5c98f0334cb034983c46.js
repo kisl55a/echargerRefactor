b4a3e16791b48081072d199b3b6dee133b177dce
Dmitrii Kislenko <dmitrii.kislenko@lianatech.com>
2020-07-14 13:57:39 +0300

    Build

---
+++ frontend/build/precache-manifest.afd72cc9ab9a5c98f0334cb034983c46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f19d78114438ce3d66a69d8157630b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "868fe6db2f625c06e1f1",
+    "url": "/static/css/main.0d8251dd.chunk.css"
+  },
+  {
+    "revision": "51eba8d057a144b45c7a",
+    "url": "/static/js/2.f26d331d.chunk.js"
+  },
+  {
+    "revision": "868fe6db2f625c06e1f1",
+    "url": "/static/js/main.362feb44.chunk.js"
+  },
+  {
+    "revision": "42eb98ea172ef39510b9",
+    "url": "/static/js/runtime-main.5441bf2f.js"
+  }
+]);
